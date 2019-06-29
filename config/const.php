3ac63ad2d9e8af4855e0754af74f738ecf40eaff
gentian0821 <gentian0821@gmail.com>
2019-06-30 00:51:11 +0900

    add config

---
+++ config/const.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'line_access_token' => '83lp46KV9G7kxQwpadkxMI6+4X6d+ByUAY/pkAqZ+QTYbmknMS13VNiXDWPNp7WorKSGwA+aRKXxdHyipGRTeZ6nX5o'
+        . '4u5t1CGa0ciTuAykVTxJ4LS4gm9+APoQFqCfLltgJGtLqzwm0fOUuTWrCYQdB04t89/1O/w1cDnyilFU=',
+
+    'line_base_uri' => 'https://api.line.me',
+
+    'line_reply_api' => '/v2/bot/message/reply',
+];
