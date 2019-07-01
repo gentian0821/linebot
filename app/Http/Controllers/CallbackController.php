cf68e0952f280b0f517b199457f2de76308d5aed
gentian0821 <gentian0821@gmail.com>
2019-07-01 22:23:20 +0900

    add list

--- app/Http/Controllers/CallbackController.php
+++ app/Http/Controllers/CallbackController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Task;
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Config;
@@ -40,7 +41,7 @@ class CallbackController extends Controller
                 'json' => [
                     "replyToken" => $param["events"][0]["replyToken"],
                     "messages" => [
-                        $this->reply_info($param['events'][0]['message']["text"])
+                        $this->reply_info($param['events'][0]['message']["text"], $param['events'][0]['source']['userId'])
                     ]
                 ],
                 'headers' => [
@@ -55,22 +56,64 @@ class CallbackController extends Controller
         return response()->json(['ok']);
     }
 
-    private function reply_info($push_text) {
-        $result = [
-            "type" => "text",
-            "text" => $push_text
-        ];
-
-        if (strpos($push_text, '登録') !== false) {
-            $result['text'] = '登録したよー！';
+    private function reply_info($push_text, $send_to)
+    {
+        $result = $this->regist($push_text, $send_to);
+        if ($result) {
+            return $result;
         }
 
-//        $result = [
-//            "type" => "sticker",
-//            "packageId" => '11538',
-//            'stickerId' => '51626531'
-//        ];
+        return [
+            'type' => 'text',
+            'text' => $push_text
+        ];
+    }
 
-        return $result;
+    private function regist($push_text, $send_to)
+    {
+        if (strpos($push_text, '登録') === false) {
+            return [];
+        }
+
+        if (!preg_match('/^(\d{1,2}\/\d{1,2}) (\d{1,2})/u',$push_text, $date_matches)) {
+            return [];
+        }
+
+        if (!preg_match('/「(.*)」/u',$push_text, $message_matches)) {
+            return [];
+        }
+
+        $task = new Task;
+        $task->send_to = $send_to;
+        $task->message = $message_matches[1];
+        $task->reserved_at = sprintf('2019-%02d-%02d %02d:00:00', $date_matches[1], $date_matches[2], $date_matches[3]);
+
+        $task->save();
+
+        Log::info($task);
+
+        return [
+            'type' => 'text',
+            'text' => '登録したよー！'
+        ];
+    }
+
+    private function fetch($push_text) {
+        if (strpos($push_text, 'リスト') === false) {
+            return [];
+        }
+
+        $tasks = Task::where('reserved_at', '>', date('Y-m-d H:i:s'))->get();
+
+        $message = 'お知らせ予定だよー\n';
+        foreach ($tasks as $task) {
+            $message .= $task->reserved_at . ' ' . $task->message . '\n';
+        }
+
+        return [
+            'type' => 'text',
+            'text' => $message
+        ];
+
     }
 }
