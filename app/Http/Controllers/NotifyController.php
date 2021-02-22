cf0f71165de54168abf57d8c19a633f1867e30a8
gentian0821 <gentian0821@gmail.com>
2021-02-22 15:52:37 +0900

    all commit

--- app/Http/Controllers/NotifyController.php
+++ app/Http/Controllers/NotifyController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use packages\UseCase\Notify\Send\NotifySendUseCaseInterface;
 
 class NotifyController extends Controller
 {
     public function index(Request $request, NotifySendUseCaseInterface $interactor)
     {
+        Log::info(print_r($request->getContent(), true));
+        Log::info(print_r($request->input('message'), true));
         $interactor->handle($request->input('message'));
 
         return response()->json(['ok']);
