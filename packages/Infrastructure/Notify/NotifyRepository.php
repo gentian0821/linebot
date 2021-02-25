05ec6470ea7f54f869690ab681bc18a5aa833f65
gentian0821 <gentian0821@gmail.com>
2021-02-25 12:41:00 +0900

    メッセージ変更

--- packages/Infrastructure/Notify/NotifyRepository.php
+++ packages/Infrastructure/Notify/NotifyRepository.php
@@ -4,6 +4,7 @@ namespace packages\Infrastructure\Notify;
 
 use App\Services\MessageApiService;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Str;
 
 class NotifyRepository implements NotifyRepositoryInterface
 {
@@ -20,12 +21,47 @@ class NotifyRepository implements NotifyRepositoryInterface
             return;
         }
 
-        $message = "From: " . $params['from'] . "\nSubject: " . $params['subject'] .
-            "\n\n" . explode('-------', $params['message'])[0];
-
         $this->messageApiService->push(
-            [['type' => 'text', 'text' => $message]],
+            [['type' => 'text', 'text' => $this->makeMessage($params)]],
             Config::get('const.fayc4_send_to')
         );
+    }
+
+    private function makeMessage(array $params): string
+    {
+        if (Str::contains('登下校', $params['subject']))
+        {
+            $message = explode('-------', $params['message'])[0];
+            if (Str::contains('登校', $message)) {
+                return 'ゆいちゃんが無事登校したよー️' . $this->picture_letter("1F604") .
+                    "\n\n" . $message;
+            }
+
+            return 'ゆいかが帰ってくるぞー❗️❗️' . "\n\n" . $message;
+        }
+
+        if (Str::contains('そろばん塾：入室通知', $params['subject']))
+        {
+            return 'ゆいちゃんが無事そろばんに行ったよー️' . $this->picture_letter("1F604") .
+                    "\n\n" . $params['message'];
+        }
+
+        if (Str::contains('そろばん塾：退室通知', $params['subject']))
+        {
+            return 'ゆいちゃんがそろばんから帰ってくるぞー️' . $this->picture_letter("1F61C") .
+                "\n\n" . $params['message'];
+        }
+
+        return $message = "From: " . $params['from'] . "\nSubject: " . $params['subject'] .
+            "\n\n" . $params['message'];
+    }
+
+
+    private function picture_letter($code)
+    {
+        // 16進エンコードされたバイナリ文字列をデコード
+        $bin = hex2bin(str_repeat('0', 8 - strlen($code)) . $code);
+        // UTF8へエンコード
+        return mb_convert_encoding($bin, 'UTF-8', 'UTF-32BE');
     }
 }
